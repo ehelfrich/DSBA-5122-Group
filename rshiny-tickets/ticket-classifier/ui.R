0aa818014670eb8b1dd38615987be3ec0d9aa0fb
Eric Helfrich <ericjhelfrich@gmail.com>
2019-12-14 10:30:28 -0500

    Overview tab update

--- rshiny-tickets/ticket-classifier/ui.R
+++ rshiny-tickets/ticket-classifier/ui.R
@@ -45,12 +45,39 @@ shinyUI(dashboardPage(
       tabItem(
         h1("Business Case"),
         tabName = "businesscase",
-        paste0(
-          "We explored typical customer ticket classification data to grasp customer word usage and ticket results. Our domain problem is one for agents who handle service tickets to quickly and efficiently classify the problem and resolve the ticket.
-          This application was created to showcase the concept of this model, using a data science team approach, to demonstrate machine learning pipeline operations for a decision-making audience.  The result will allow decision makers to determine if the feature engineering steps and model results are a worth-while investment to “productionize” the pipeline.
-          "
-        )
-        ),
+        fluidRow(
+          box(h2("Domain Problem"),
+              p("Our domain problem is one for agents who handle service tickets to quickly and efficiently classify the problem and resolve the ticket.
+                In a system without an automated classification system, agents would have to assign categories dependent on many variables,
+                such as content and priority, and who to send tickets to. Many times, tickets can be classified incorrectly, which could be caused on accident,
+                faulty information, or when the end user gives up trying to classify the ticket. Once this agent misclassifies the ticket,
+                it is sent on to the next service desk, incorrectly, to fix and therefore wasting precious time for the next handler of the ticket.
+                At a larger company, this happens multiple times a day."),
+              h2("Purpose of the App"),
+              p("This application was created to showcase the concept of a model, using a data science team approach,
+                to demonstrate machine learning pipeline operations for a decision-making audience.
+                The result will allow decision makers to determine if the feature engineering steps and model results are a worth-while investment
+                to “productionize” the pipeline."),
+              hr(),
+              h2("App Contents"),
+              h3("Data Exploration"),
+              p("The Data Exploration tab allows the user to generate a training dataset.  
+                The testing set is generated at application start and remains static throughout the entire session.  
+                The tab shows a data table of generated training set, summary metrics of the test/train datasets, 
+                and the distribution of categories for the training set.  "),
+              h3("Feature Engineering"),
+              p("The Feature Engineering tab gives the user the ability to apply different transformations to the dataset and then visualize
+                how it affects the words in the dataset.  The word counts histogram and the word cloud allow the user to visualize how the stopwords/minimum words
+                in a ticket affect the most common words."),
+              h3("Dimensionality Reduction"),
+              p("The Dimensionality Reduction tab allows the user to reduce the huge number of dimensions in the dataset down to 2 dimensions. 
+                These reduced dimensions can then be viewed in a 2-d plot that can be visualized by humans. 
+                This will help users visualize possible clusters of tickets in the data set and possibly see patterns between tickets."),
+              h3("Machine Learning"),
+              p("The Machine Learning tab allows the user to create a random forest classification model to predict the ticket category based on the body of the ticket. 
+                It then shows  the model metrics and the confusion matrix after the model as been used to predict the test set’s categories."))
+          )
+      ),
       #### Data Exploration Tab ####
       tabItem(h1("Data Exploration"), tabName = "dataexploration",
               fluidRow(
